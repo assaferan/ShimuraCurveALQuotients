910b446151e0d82b7bbdf9c99b2eef9bc38b5eb4
Eran Assaf <assaferan@gmail.com>
2024-12-02 12:18:04 -0500

    Evaluation at CM points passes the delta test

--- ExpansionOfModularForms.m
+++ ExpansionOfModularForms.m
@@ -1,12 +1,15 @@
+// This implements the method desrcibed in [Nel15]. (Paul Nelson)
+// Should also try to implement [VW] (Voight-Willis)
+
 function W(k, x, prec)
     s := 0;
     for n in [1..prec] do
-	s +:= n^k*(n*x*KBessel(k-1, n*x) - KBessel(k, n*x));
+	    s +:= n^k*(n*x*KBessel(k-1, n*x) - KBessel(k, n*x));
     end for;
     return 2^(1-k)*s;
 end function;
 
-function V(k, Lbasis, t, Bound, Wprec)
+function V(k, Lbasis, t, Bound, Wprec, eps)
     RR := RealField();
     pi := Pi(RR);
     M2F := Universe(Lbasis);
@@ -14,22 +17,23 @@ function V(k, Lbasis, t, Bound, Wprec)
     F := BaseRing(M2F);
     // We write it this way to make it easier to change the norm if we want to
     nm := map<M2F -> F | x :-> x[1,1]^2+x[2,2]^2 + x[1,2]^2 + x[2,1]^2 >;
-    X := map<M2F -> F | x :-> (x[1,1]+x[2,2])^2 + (x[1,2] - x[2,1])^2 >;
+    abs4X2 := map<M2F -> F | x :-> (x[1,1]+x[2,2])^2 + (x[1,2] - x[2,1])^2 >;
     Lgram := Matrix([[nm(x+y)-nm(x)-nm(y) : y in Lbasis] : x in Lbasis]);
     // Lgram := ChangeRing(Lgram, Rationals());
     L := LatticeWithGram(Lgram);
-    nmBd := 4*Bound^2; // if |X|^2 = (a+d)^2 + (b-c)^2 < Bound, then
-                       // a^2 + b^2 + c^2 + d^2 < 4*Bound^2;
+    nmBd := Bound; // if 4*|X|^2 = (a+d)^2 + (b-c)^2 < Bound, then
+                   // a^2 + b^2 + c^2 + d^2 < 4*|X|^2 < Bound;
     SV := ShortVectors(L, nmBd);
     alphas := [&+[v[1][i]*Lbasis[i] : i in [1..4]] : v in SV];
-    alphas_det := [alpha : alpha in alphas | Determinant(alpha) eq t];
+    alphas_det := [alpha : alpha in alphas | Abs(Determinant(alpha) - t) lt eps];
     s := 0;
     for alpha in alphas_det do
-	Xalpha := RR!X(alpha);
-	root_num := (Xalpha/ Abs(Xalpha))^k;
-	s +:= root_num * W(k, 4*pi*Abs(Xalpha), Wprec);
+        a,b,c,d := Explode(Eltseq(alpha));
+        Xalpha := 1/2*(a+d+i*(b-c));
+	    root_num := (Xalpha/ Abs(Xalpha))^k;
+	    s +:= root_num * W(k, 4*pi*Abs(Xalpha), Wprec);
     end for;
-    return s;
+    return 2*s; // because SV contains only one representative from each pair {alpha, -alpha}
 end function;
 
 function FindRdbases(D,N)
@@ -38,12 +42,12 @@ function FindRdbases(D,N)
     // need B to be indefinite
     assert exists(a){a : a in [i,j] | ZZ!(a^2) gt 0};
     if D eq 1 then
-	emb_B_BF := hom<B -> B | [1,i,j,i*j]>;
-	is_mat, _, iota := IsMatrixRing(B : Isomorphism);
+	    emb_B_BF := hom<B -> B | [1,i,j,i*j]>;
+	    is_mat, _, iota := IsMatrixRing(B : Isomorphism);
     else
-	F := QuadraticField(ZZ!(a^2));
-	BF, emb_B_BF := ChangeRing(B, F);
-	is_mat, _, iota := IsMatrixRing(BF : Isomorphism);
+	    F := QuadraticField(ZZ!(a^2));
+	    BF, emb_B_BF := ChangeRing(B, F);
+	    is_mat, _, iota := IsMatrixRing(BF : Isomorphism);
     end if;
     assert is_mat; // F should split B
     O := Order(MaximalOrder(B), N);
@@ -55,30 +59,31 @@ function FindRdbases(D,N)
     basis := [&+[hnf[i,j]*dual_basis[j] : j in [1..4]] : i in [1..4]];
     Rdbases := AssociativeArray();
     for d in Divisors(D*N) do
-	Rdbases[d] := [(1/GCD(d, hnf[i,i]))*basis[i] : i in [1..4]];
-	// we actually want to embd them into M2(R)
-	Rdbases[d] := [iota(emb_B_BF(b)) : b in Rdbases[d]];
+	    Rdbases[d] := [(1/GCD(d, hnf[i,i]))*basis[i] : i in [1..4]];
+	    // we actually want to embd them into M2(R)
+	    Rdbases[d] := [iota(emb_B_BF(b)) : b in Rdbases[d]];
     end for;
     return Rdbases;
 end function;
 
 // For now we only do maximal order, N = 1
-function EvaluateNormAti(f, prec, Vbound, Wprec, Rdbases)
+function EvaluateNormAti(f, prec, Vbound, Wprec, eps, Rdbases)
     D := Level(f);
     k := Weight(f);
     s := 0;
     for n in [1..prec] do
-	inner_sum := 0;
-	for j->d in Divisors(D) do
-	    c := 1/(d*Coefficient(f,d));
-	    inner_sum +:= c*V(k, Rdbases[j], n/d, Vbound, Wprec);
-	end for;
-	s +:= Coefficient(f,n)*inner_sum;
+        inner_sum := 0;
+        for j->d in Divisors(D) do
+            c := 1/(d*Coefficient(f,d));
+            inner_sum +:= c*V(k, Rdbases[j], n/d, Vbound, Wprec, eps);
+        end for;
+	    s +:= Coefficient(f,n)*inner_sum;
     end for;
     return s;
 end function;
 
-function EvaluateNorm(D,N,f,z, prec, Vbound, Wprec)
+// This is [Nel15, Thm 3.1]
+function EvaluateNorm(D,N,f,z, prec, Vbound, Wprec, eps)
     x := Re(z);
     y := Im(z);
     sqrty := Sqrt(y);
@@ -88,13 +93,27 @@ function EvaluateNorm(D,N,f,z, prec, Vbound, Wprec)
     Rdbases := FindRdbases(D,N);
     s := 0;
     for n in [1..prec] do
-	inner_sum := 0;
-	for d in Divisors(D*N) do
-	    c := MoebiusMu(GCD(d,N))/(d*Coefficient(f,d));
-	    basis := [sigma^(-1)*m*sigma : m in Rdbases[d]];
-	    inner_sum +:= c*V(k, basis, n/d, Vbound, Wprec);
-	end for;
-	s +:= Coefficient(f,n)*inner_sum;
+        inner_sum := 0;
+        for d in Divisors(D*N) do
+            c := MoebiusMu(GCD(d,N))/(d*Coefficient(f,d));
+            basis := [sigma^(-1)*m*sigma : m in Rdbases[d]];
+            inner_sum +:= c*V(k, basis, n/d, Vbound, Wprec, eps);
+        end for;
+        s +:= Coefficient(f,n)*inner_sum;
     end for;
     return y^(-k)*s;
 end function;
+
+procedure testDelta()
+    delta := Basis(CuspForms(1,12))[1];
+    delta_prec := 9;
+    CC<i> := ComplexField();
+    z := i;
+    val := Evaluate(qExpansion(delta, delta_prec), Exp(2*Pi(CC)*i*z))^2;
+    RdBases := FindRdbases(1,1);
+    eps := 10^(-5);
+    val2 := EvaluateNormAti(delta, 10, 100, 5, eps, RdBases);
+    assert Abs(val - val2) lt eps;
+    val3 := EvaluateNorm(1,1,delta,z,10,100,5,eps);
+    assert Abs(val - val3) lt eps;
+end procedure;
