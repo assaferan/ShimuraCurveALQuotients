138aab93eb5b780a882ea36582722314e05e9934
sachihashimoto <svh@bu.edu>
2024-12-13 16:55:37 -0500

    fix code

--- involutioncode.m
+++ involutioncode.m
@@ -1,3 +1,13 @@
+    curves := GetHyperellipticCandidates();
+    unknown := [c : c in curves | not assigned c`IsSubhyp];
+    unknownstar := [ c : c in unknown | IsStarCurve(c)];
+    unknownmodstar := [ c : c in unknownstar | c`D eq 1];
+    lut := AssociativeArray();
+    for i in [1..#curves] do
+        c := curves[i];
+        lut[<c`D, c`N, c`W>] := i;
+    end for;
+
 potential_curves := [];
 for i->X in unknownstar do
     print "doing curve number", i;
