6c43258bc82bf36eb5d2df1f5584858142afe237
Eran Assaf <assaferan@gmail.com>
2025-12-15 13:39:29 -0500

    modifying the test for 6,17 so that the example matches the variables in Guo-Yang

--- tests/X0_6_17.m
+++ tests/X0_6_17.m
@@ -4,12 +4,12 @@ function load_covers_and_ws_data_6_17()
     P3<x,y,z,s> := WeightedProjectiveSpace(Rationals(), [1,2,1,1]);
      // D = 82
     cover_data := AssociativeArray();
-    cover_data[{1}] := <Curve(P3, [y^2 - 17*s^4 + 10*s^2*z^2 - 9*z^4, x^2 + 3*s^2 + 16*z^2]), DiagonalMatrix([3,4,1,1])>;
+    cover_data[{1}] := <Curve(P3, [y^2 - 17*x^4 + 10*x^2*s^2 - 9*s^4, z^2 + 3*x^2 + 16*s^2]), Matrix([[0,0,3,0],[0,4,0,0],[0,0,0,1],[1,0,0,0]])>;
 
     ws_data := AssociativeArray();
     ws_data[{1}] := AssociativeArray();
-    ws_data[{1}][2] := DiagonalMatrix([1,1,1,-1]);
-    ws_data[{1}][3] := DiagonalMatrix([-1,-1,1,1]);
+    ws_data[{1}][2] := DiagonalMatrix([-1,1,1,1]);
+    ws_data[{1}][3] := DiagonalMatrix([1,-1,-1,1]);
     ws_data[{1}][34] := DiagonalMatrix([1,-1,1,1]);
 
     return cover_data, ws_data;
