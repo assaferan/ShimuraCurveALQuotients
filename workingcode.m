448d3ee04ed3b5e1b595d04ccf4349e8d9b599ed
Eran Assaf <assaferan@gmail.com>
2026-05-28 09:54:45 -0400

    adding test for running everything

--- workingcode.m
+++ workingcode.m
@@ -24,27 +24,27 @@ intrinsic GetHyperellipticCandidates(:recompute_data:=false, read_data :=true) -
         assert r eq 7;
         // Find all pairs (D,N) satisfying the inequality of
         // Proposition 1.
-        t0 := RealTime();
+        t0 := Realtime();
         star_curves := FindPairs(r); // time : 1.980
-        print "FindPairs took ", RealTime() - t0;
+        print "FindPairs took ", Realtime() - t0;
         // I added some code that just
         // focuses on the star quotients X_0^*(D,N)
         assert #star_curves eq 2342;
-        t0 := RealTime();
+        t0 := Realtime();
         UpdateGenera(~star_curves); // time: 12
-        print "UpdateGenera for star curves took ", RealTime() - t0;
+        print "UpdateGenera for star curves took ", Realtime() - t0;
         VerifyHHTable1(star_curves);
         UpdateByGenus(~star_curves);
-        t0 := RealTime();
+        t0 := Realtime();
         FilterByTrace(~star_curves); // time : 2850.270
-        print "FilterByTrace for star curves took ", RealTime() - t0;
+        print "FilterByTrace for star curves took ", Realtime() - t0;
         Write("data/star_curves_point_count.dat", Sprint(star_curves, "Magma") : Overwrite);
         VerifyHHTable2(star_curves);
         HHProposition1(~star_curves);
         VerifyHHProposition1(star_curves);
-        t0 := RealTime();
+        t0 := Realtime();
         FilterStarCurvesByFpAutomorphisms(~star_curves, 10, 20);
-        print "FilterStarCurvesByFpAutomorphisms took ", RealTime() - t0;
+        print "FilterStarCurvesByFpAutomorphisms took ", Realtime() - t0;
         // Create a list of all Atkin-Lehner quotients
         // compute their genera, and store the covering structure.
         // writing to a file, in case we would like to load it directly
@@ -52,58 +52,58 @@ intrinsic GetHyperellipticCandidates(:recompute_data:=false, read_data :=true) -
         read_curves := eval Read("data/star_curves_after_FpAuto.dat");
         assert #read_curves eq #star_curves;
         assert &and[(read_curves[j] eq star_curves[j]) : j in [1..#star_curves]];
-        t0 := RealTime();
+        t0 := Realtime();
         curves := GetQuotientsAndGenera(star_curves); // 61.520
-        print "GetQuotientsAndGenera took ", RealTime() - t0;
+        print "GetQuotientsAndGenera took ", Realtime() - t0;
         // updating classification from the genera we computed
-        t0 := RealTime();
+        t0 := Realtime();
         UpdateByGenus(~curves);
-        print "UpdateByGenus took ", RealTime() - t0;
+        print "UpdateByGenus took ", Realtime() - t0;
         UpdateCurves(~curves);
         Write("data/all_curves_after_genus.dat", Sprint(curves, "Magma") : Overwrite);
         // Using the fact that if w acts non-trivially and has more than
         // 4 fixed points on X, then X is non-hyperelliptic
-        t0 := RealTime();
+        t0 := Realtime();
         FilterByALFixedPointsOnQuotient(~curves);
-        print "FilterByALFixedPointsOnQuotient took ", RealTime() - t0;
+        print "FilterByALFixedPointsOnQuotient took ", Realtime() - t0;
         UpdateCurves(~curves);
         Write("data/all_curves_after_ALFixed.dat", Sprint(curves, "Magma") : Overwrite);
         // if a genus 3 covers a genus 2 curve, then it is hyperelliptic
-        t0 := RealTime();
+        t0 := Realtime();
         Genus3CoversGenus2(~curves);
-        print "Genus3CoversGenus2 took ", RealTime() - t0;
+        print "Genus3CoversGenus2 took ", Realtime() - t0;
         UpdateCurves(~curves);
         Write("data/all_curves_after_genus3.dat", Sprint(curves, "Magma") : Overwrite);
-        t0 := RealTime();
+        t0 := Realtime();
         FilterByDegeneracyMorphism(~curves);
-        print "FilterByDegeneracyMorphism took ", RealTime() - t0;
+        print "FilterByDegeneracyMorphism took ", Realtime() - t0;
         UpdateCurves(~curves);
         Write("data/all_curves_after_degeneracy.dat", Sprint(curves, "Magma") : Overwrite);
         // Using Proposition 6 from [FH] adapted to the Shimura curve situation
-        t0 := RealTime();
+        t0 := Realtime();
         FilterByComplicatedALFixedPointsOnQuotient(~curves); // 64.840
-        print "FilterByComplicatedALFixedPointsOnQuotient took ", RealTime() - t0;
+        print "FilterByComplicatedALFixedPointsOnQuotient took ", Realtime() - t0;
         UpdateCurves(~curves);
         Write("data/all_curves_after_ComplicatedALFixed.dat", Sprint(curves, "Magma") : Overwrite);
 
-        t0 := RealTime();
+        t0 := Realtime();
         FilterByNonALInvolutions(~curves);
-        print "FilterByNonALInvolutions took ", RealTime() - t0;      
+        print "FilterByNonALInvolutions took ", Realtime() - t0;      
         UpdateCurves(~curves);
         Write("data/all_curves_after_NonALInvolutions.dat", Sprint(curves, "Magma") : Overwrite);
 
         // Using trace of Hecke operators to count points and show more curves are
         // non-hyperelliptic
-        t0 := RealTime();
+        t0 := Realtime();
         FilterByTrace(~curves);
-        print "FilterByTrace took ", RealTime() - t0;
-        t0 := RealTime();
+        print "FilterByTrace took ", Realtime() - t0;
+        t0 := Realtime();
         UpdateCurves(~curves); // 13.750
-        print "UpdateCurves took ", RealTime() - t0;
+        print "UpdateCurves took ", Realtime() - t0;
         Write("data/all_curves_after_trace.dat", Sprint(curves, "Magma") : Overwrite);
-        t0 := RealTime();
+        t0 := Realtime();
         FilterByWeilPolynomial(~curves);
-        print "FilterByWeilPolynomial took ", RealTime() - t0;
+        print "FilterByWeilPolynomial took ", Realtime() - t0;
         UpdateCurves(~curves);
         Write("data/all_curves_after_Weil.dat", Sprint(curves, "Magma") : Overwrite);
     end if;
