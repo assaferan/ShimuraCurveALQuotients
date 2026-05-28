b14376a11c6d23b5546b66e0a9b69cde70c142e2
Eran Assaf <assaferan@gmail.com>
2026-05-28 09:10:12 -0400

    gardening

--- workingcode.m
+++ workingcode.m
@@ -1,4 +1,16 @@
-// AttachSpec("ShimuraQuotients.spec");
+// This small procedure is used to update
+// the curves after every filter by using
+// upward and downward closures, as well as isomorphisms
+procedure UpdateCurves(~curves)
+  UpdateByIsomorphisms(~curves);
+  // upward closure - if X-->Y is dominant then gon(X) >= gon(Y).
+  // [Poonen, A.1.(vii)]
+  // In particular, if Y is non-hyperelliptic, so is X.
+  UpwardClosure(~curves);
+  // downward closure - if covered by subhyperelliptic, then subhyperelliptic
+  DownwardClosure(~curves);
+end procedure;
+
 intrinsic GetHyperellipticCandidates(:recompute_data:=false, read_data :=true) -> SeqEnum
 {.}
     // SetDebugOnError(true);
@@ -9,132 +21,66 @@ intrinsic GetHyperellipticCandidates(:recompute_data:=false, read_data :=true) -
 
     if recompute_data then 
         r := GetLargestPrimeIndex();
-
         assert r eq 7;
-
         // Find all pairs (D,N) satisfying the inequality of
         // Proposition 1.
         time star_curves := FindPairs(r); // time : 1.980
-
         // I added some code that just
         // focuses on the star quotients X_0^*(D,N)
-
         assert #star_curves eq 2342;
-
         time UpdateGenera(~star_curves); // time: 12
-
         VerifyHHTable1(star_curves);
-
         UpdateByGenus(~star_curves);
-
         FilterByTrace(~star_curves); // time : 2850.270
-
         VerifyHHTable2(star_curves);
-
         // Create a list of all Atkin-Lehner quotients
         // compute their genera, and store the covering structure.
-
         // writing to a file, in case we would like to load it directly
-
         Write("data/star_curves_point_count.dat", Sprint(star_curves, "Magma") : Overwrite);
-
         read_curves := eval Read("data/star_curves_point_count.dat");
         assert #read_curves eq #star_curves;
         assert &and[(read_curves[j] eq star_curves[j]) : j in [1..#star_curves]];
         HHProposition1(~star_curves);
         VerifyHHProposition1(star_curves);
+        unknownstar := [ c : c in star_curves | not assigned c`IsSubhyp];
+        for p in PrimesUpTo(10) do
+            FilterStarCurvesByFpAutomorphisms(unknownstar, ~star_curves, p, 20 );
+        end for;
 
         time curves := GetQuotientsAndGenera(star_curves); // 61.520
-
         // updating classification from the genera we computed
         UpdateByGenus(~curves);
-
-        // upward closure - if X-->Y is dominant then gon(X) >= gon(Y). [Poonen, A.1.(vii)]
-        // In particular, if Y is non-hyperelliptic, so is X.
-        UpwardClosure(~curves);
-
-        // downward closure - if covered by subhyperelliptic, then subhyperelliptic
-        DownwardClosure(~curves);
-
+        UpdateCurves(~curves);
         // Using the fact that if w acts non-trivially and has more than
         // 4 fixed points on X, then X is non-hyperelliptic
         FilterByALFixedPointsOnQuotient(~curves);
-
-        // upward closure - if covering a non-hyperelliptic, then non-hyperelliptic
-        UpwardClosure(~curves);
-
+        UpdateCurves(~curves);
         // if a genus 3 covers a genus 2 curve, then it is hyperelliptic
         Genus3CoversGenus2(~curves);
-
-        DownwardClosure(~curves);
-
+        UpdateCurves(~curves);
         // Using Proposition 6 from [FH] adapted to the Shimura curve situation
-
         time FilterByComplicatedALFixedPointsOnQuotient(~curves); // 64.840
-
-        UpwardClosure(~curves);
-
+        UpdateCurves(~curves);
         // Using trace of Hecke operators to count points and show more curves are
         // non-hyperelliptic
         time FilterByTrace(~curves);
-
-        UpwardClosure(~curves);
-
-        time UpdateByIsomorphisms(~curves); // 13.750
-
-        UpwardClosure(~curves);
-
-        DownwardClosure(~curves);
-
+        time UpdateCurves(~curves); // 13.750
+        Write("data/all_curves_progress.dat", Sprint(curves, "Magma") : Overwrite);
         Write("data/all_curves_progress.dat", Sprint(curves, "Magma") : Overwrite);
 
-        FilterByWSPoints(~curves);
-        
-        UpdateByIsomorphisms(~curves);
 
-        UpwardClosure(~curves);
-
-        DownwardClosure(~curves);
+        UpdateCurves(~curves);
 
         Write("data/all_curves_progress.dat", Sprint(curves, "Magma") : Overwrite);
-
-        unknownstar := [ c : c in curves | IsStarCurve(c) and not assigned c`IsSubhyp];
-
-        for p in PrimesUpTo(10) do
-            FilterStarCurvesByFpAutomorphisms(unknownstar, ~curves, p, 20 );
-        end for;
-
-        UpdateByIsomorphisms(~curves);
-
-        UpwardClosure(~curves);
-
-        DownwardClosure(~curves);
-
-        Write("data/all_curves_progress.dat", Sprint(curves, "Magma") : Overwrite);
-
         // Was actually never run!
-        // FilterByWeilPolynomial(~curves);
-        FilterByWeilPolynomial(~curves : genera := {3,4,5});
+        FilterByWeilPolynomial(~curves);
+        // FilterByWeilPolynomial(~curves : genera := {3,4,5});
 
-        UpdateByIsomorphisms(~curves);
-
-        UpwardClosure(~curves);
-
-        DownwardClosure(~curves);
-
+        UpdateCurves(~curves);
         Write("data/all_curves_progress.dat", Sprint(curves, "Magma") : Overwrite);
-
         FilterByDegeneracyMorphism(~curves);
-
-        UpdateByIsomorphisms(~curves);
-
-        UpwardClosure(~curves);
-
-        DownwardClosure(~curves);
-
+        UpdateCurves(~curves);
         Write("data/all_curves_progress.dat", Sprint(curves, "Magma") : Overwrite);
-
-
     end if;
 
     if read_data then
