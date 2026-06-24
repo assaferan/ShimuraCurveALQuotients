d114a43547275375405d7cfc1d75fb149d87eae3
sachihashimoto <svh@bu.edu>
2026-06-24 10:57:42 -0400

    revert to old fieldsofdefinition, which is faster

--- tests/CMPoints.m
+++ tests/CMPoints.m
@@ -125,7 +125,7 @@ procedure test_FieldOfDefinition()
         vprintf ShimuraQuotients, 1: "testing points on %o...", X;
         for pt in cm do
             d, K := Explode(pt);
-            Fs := FieldsOfDefinitionOfCMPointFast(X, d);
+            Fs := FieldsOfDefinitionOfCMPoint(X, d);
             assert exists(F){F : F in Fs | IsIsomorphic(F,K)};
         end for;
         vprintf ShimuraQuotients, 1: "Done!\n";
